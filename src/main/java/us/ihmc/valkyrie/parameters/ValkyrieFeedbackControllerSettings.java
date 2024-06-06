30aea0c3286a03c1035252329cf4fd75c0a61ebc
Sylvain <sbertrand@ihmc.org>
2024-06-06 16:14:09 -0500

    Collapsed the old low-pass filter in the new setup

--- src/main/java/us/ihmc/valkyrie/parameters/ValkyrieFeedbackControllerSettings.java
+++ src/main/java/us/ihmc/valkyrie/parameters/ValkyrieFeedbackControllerSettings.java
@@ -1,24 +1,32 @@
 package us.ihmc.valkyrie.parameters;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import us.ihmc.avatar.drcRobot.RobotTarget;
-import us.ihmc.commonWalkingControlModules.configurations.GroupParameter;
+import us.ihmc.commonWalkingControlModules.momentumBasedController.feedbackController.FeedbackControllerFilterFactory;
 import us.ihmc.commonWalkingControlModules.momentumBasedController.feedbackController.FeedbackControllerSettings;
-import us.ihmc.robotics.partNames.ArmJointName;
-import us.ihmc.robotics.robotSide.RobotSide;
+import us.ihmc.yoVariables.registry.YoRegistry;
+import us.ihmc.yoVariables.variable.YoDouble;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class ValkyrieFeedbackControllerSettings implements FeedbackControllerSettings
 {
-   private final ValkyrieJointMap jointMap;
-   private final RobotTarget target;
+   private YoDouble pelvisVelocityErrorBreakFrequency;
+   private YoDouble chestVelocityErrorBreakFrequency;
+   private YoDouble footVelocityErrorBreakFrequency;
+   private YoDouble armJointsVelocityErrorBreakFrequency;
+
+   private final String pelvisName;
+   private final String chestName;
+   private final Set<String> footNames;
+   private final Set<String> armJointNames;
 
    public ValkyrieFeedbackControllerSettings(ValkyrieJointMap jointMap, RobotTarget target)
    {
-      this.jointMap = jointMap;
-      this.target = target;
+      pelvisName = jointMap.getPelvisName();
+      chestName = jointMap.getChestName();
+      footNames = new HashSet<>(jointMap.getFootNames());
+      armJointNames = new HashSet<>(jointMap.getArmJointNamesAsStrings());
    }
 
    @Override
@@ -28,24 +36,74 @@ public class ValkyrieFeedbackControllerSettings implements FeedbackControllerSet
    }
 
    @Override
-   public List<GroupParameter<Double>> getErrorVelocityFilterBreakFrequencies()
+   public FilterDouble1D getVelocity1DErrorFilter(String jointName, double dt, YoRegistry registry)
    {
-      if (target == RobotTarget.SCS)
-         return null;
-
-      GroupParameter<Double> pelvisGroup = new GroupParameter<>(jointMap.getPelvisName(), 25.0);
-      GroupParameter<Double> chestGroup = new GroupParameter<>(jointMap.getChestName(), 16.0);
-      GroupParameter<Double> footGroup = new GroupParameter<>("foot", 16.0, new ArrayList<>());
-      GroupParameter<Double> armJointsGroup = new GroupParameter<>("armJoints", 25.0, new ArrayList<>());
-
-      for (RobotSide robotSide : RobotSide.values)
+      if (armJointNames.contains(jointName))
       {
-         footGroup.getMemberNames().add(jointMap.getFootName(robotSide));
+         if (armJointsVelocityErrorBreakFrequency == null)
+         {
+            armJointsVelocityErrorBreakFrequency = new YoDouble(jointName + "VelocityErrorBreakFrequency", registry);
+            armJointsVelocityErrorBreakFrequency.set(25.0);
+         }
 
-         for (ArmJointName armJointName : jointMap.getArmJointNames())
-            armJointsGroup.getMemberNames().add(jointMap.getArmJointName(robotSide, armJointName));
+         return FeedbackControllerFilterFactory.createVelocity1DErrorLPFFilter(jointName, armJointsVelocityErrorBreakFrequency, dt, registry);
+      }
+      return null;
+   }
+
+   @Override
+   public FilterVector3D getAngularVelocity3DErrorFilter(String endEffectorName, double dt, YoRegistry registry)
+   {
+      if (endEffectorName.equals(pelvisName))
+      {
+         if (pelvisVelocityErrorBreakFrequency == null)
+         {
+            pelvisVelocityErrorBreakFrequency = new YoDouble(pelvisName + "VelocityErrorBreakFrequency", registry);
+            pelvisVelocityErrorBreakFrequency.set(25.0);
+         }
+
+         return FeedbackControllerFilterFactory.createAngularVelocityErrorLPFFilter(endEffectorName, pelvisVelocityErrorBreakFrequency, dt, registry);
       }
 
-      return Arrays.asList(pelvisGroup, chestGroup, footGroup, armJointsGroup);
+      if (endEffectorName.equals(chestName))
+      {
+         if (chestVelocityErrorBreakFrequency == null)
+         {
+            chestVelocityErrorBreakFrequency = new YoDouble(chestName + "VelocityErrorBreakFrequency", registry);
+            chestVelocityErrorBreakFrequency.set(16.0);
+         }
+
+         return FeedbackControllerFilterFactory.createAngularVelocityErrorLPFFilter(endEffectorName, chestVelocityErrorBreakFrequency, dt, registry);
+      }
+
+      if (footNames.contains(endEffectorName))
+      {
+         if (footVelocityErrorBreakFrequency == null)
+         {
+            footVelocityErrorBreakFrequency = new YoDouble(endEffectorName + "VelocityErrorBreakFrequency", registry);
+            footVelocityErrorBreakFrequency.set(16.0);
+         }
+
+         return FeedbackControllerFilterFactory.createAngularVelocityErrorLPFFilter(endEffectorName, footVelocityErrorBreakFrequency, dt, registry);
+      }
+
+      return null;
+   }
+
+   @Override
+   public FilterVector3D getLinearVelocity3DErrorFilter(String endEffectorName, double dt, YoRegistry registry)
+   {
+      if (footNames.contains(endEffectorName))
+      {
+         if (footVelocityErrorBreakFrequency == null)
+         {
+            footVelocityErrorBreakFrequency = new YoDouble(endEffectorName + "VelocityErrorBreakFrequency", registry);
+            footVelocityErrorBreakFrequency.set(16.0);
+         }
+
+         return FeedbackControllerFilterFactory.createLinearVelocityErrorLPFFilter(endEffectorName, footVelocityErrorBreakFrequency, dt, registry);
+      }
+
+      return null;
    }
 }
