be6274ab385bc210cc16d2608ea33beebd47d8a6
Stephen McCrory <smccrory1024@gmail.com>
2024-09-03 10:19:02 -0500

    Switching ihmcOpenRoboticsSoftwareVersion back to source

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ ihmc {
    configurePublications()
 }
 
-val ihmcOpenRoboticsSoftwareVersion = "0.14.0-240126"
+val ihmcOpenRoboticsSoftwareVersion = "source"
 
 mainDependencies {
    api("com.martiansoftware:jsap:2.1")
