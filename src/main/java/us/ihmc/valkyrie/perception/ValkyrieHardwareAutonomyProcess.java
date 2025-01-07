132c6dedb722288e46239b78dc469824e0e4d912
lpenco <lpenco@ihmc.org>
2025-01-07 10:19:30 -0600

    removed zed stuff

--- src/main/java/us/ihmc/valkyrie/perception/ValkyrieHardwareAutonomyProcess.java
+++ src/main/java/us/ihmc/valkyrie/perception/ValkyrieHardwareAutonomyProcess.java
@@ -1,22 +1,14 @@
 package us.ihmc.valkyrie.perception;
 
-import us.ihmc.sensors.ZEDImageSensor;
-import us.ihmc.sensors.ZEDModelData;
-import us.ihmc.zed.global.zed;
-
 public class ValkyrieHardwareAutonomyProcess
 {
-   private final ZEDImageSensor zedSensor;
-
    private final ValkyrieAutonomyProcess perceptionAutonomyProcess;
 
    private ValkyrieHardwareAutonomyProcess()
    {
-      zedSensor = new ZEDImageSensor(0, ZEDModelData.ZED_2I, zed.SL_INPUT_TYPE_USB);
-
       Runtime.getRuntime().addShutdownHook(new Thread(this::close, getClass().getSimpleName() + "Closer"));
 
-      perceptionAutonomyProcess = new ValkyrieAutonomyProcess(zedSensor);
+      perceptionAutonomyProcess = new ValkyrieAutonomyProcess();
    }
 
    private void close()
