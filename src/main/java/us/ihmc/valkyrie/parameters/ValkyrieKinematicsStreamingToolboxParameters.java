c9b9ddc7c892b019cbf74d71e1645eb029d4a377
lpenco <lpenco@ihmc.org>
2025-02-28 03:44:28 -0600

    formatting

--- src/main/java/us/ihmc/valkyrie/parameters/ValkyrieKinematicsStreamingToolboxParameters.java
+++ src/main/java/us/ihmc/valkyrie/parameters/ValkyrieKinematicsStreamingToolboxParameters.java
@@ -49,29 +49,29 @@ public class ValkyrieKinematicsStreamingToolboxParameters extends KinematicsStre
 
          // Restrict joint limits
          robotModel.setRobotDefinitionMutator(robotDefinition ->
-          {
-             OneDoFJointDefinition spineYaw = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getSpineJointName(SpineJointName.SPINE_YAW));
-             spineYaw.setPositionLimits(Math.toRadians(-45.0), Math.toRadians(45.0));
-             OneDoFJointDefinition spineRoll = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getSpineJointName(SpineJointName.SPINE_ROLL));
-             spineRoll.setPositionLimits(-0.1, 0.1);
-             OneDoFJointDefinition spinePitch = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getSpineJointName(SpineJointName.SPINE_PITCH));
-             spinePitch.setPositionLimits(-0.1, 0.4);
+         {
+            OneDoFJointDefinition spineYaw = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getSpineJointName(SpineJointName.SPINE_YAW));
+            spineYaw.setPositionLimits(Math.toRadians(-45.0), Math.toRadians(45.0));
+            OneDoFJointDefinition spineRoll = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getSpineJointName(SpineJointName.SPINE_ROLL));
+            spineRoll.setPositionLimits(-0.1, 0.1);
+            OneDoFJointDefinition spinePitch = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getSpineJointName(SpineJointName.SPINE_PITCH));
+            spinePitch.setPositionLimits(-0.1, 0.4);
 
-             for (RobotSide robotSide : RobotSide.values)
-             {
-                OneDoFJointDefinition shoulderPitch = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH));
-                shoulderPitch.setPositionLimits(-1.5, 0.8);
+            for (RobotSide robotSide : RobotSide.values)
+            {
+               OneDoFJointDefinition shoulderPitch = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getArmJointName(robotSide, ArmJointName.SHOULDER_PITCH));
+               shoulderPitch.setPositionLimits(-1.5, 0.8);
 
-                OneDoFJointDefinition shoulderRoll = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL));
-                if (robotSide == RobotSide.LEFT)
-                   shoulderRoll.setPositionLimits(-1.3, 0.3);
-                else
-                   shoulderRoll.setPositionLimits(-0.3, 1.3);
+               OneDoFJointDefinition shoulderRoll = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getArmJointName(robotSide, ArmJointName.SHOULDER_ROLL));
+               if (robotSide == RobotSide.LEFT)
+                  shoulderRoll.setPositionLimits(-1.3, 0.3);
+               else
+                  shoulderRoll.setPositionLimits(-0.3, 1.3);
 
-                OneDoFJointDefinition shoulderYaw = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getArmJointName(robotSide, ArmJointName.SHOULDER_YAW));
-                shoulderYaw.setPositionLimits(-1.5, 1.5);
-             }
-          });
+               OneDoFJointDefinition shoulderYaw = robotDefinition.getOneDoFJointDefinition(robotModel.getJointMap().getArmJointName(robotSide, ArmJointName.SHOULDER_YAW));
+               shoulderYaw.setPositionLimits(-1.5, 1.5);
+            }
+         });
       }
    }
 
