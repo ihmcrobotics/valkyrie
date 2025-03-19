4c66953adb5e77e8bf20eaf7190eee7a223f2104
rgriffin@ihmc.org <rgriffin@ihmc.us>
2025-03-19 14:42:30 -0500

    finished moving the yo variable filters

--- src/main/java/us/ihmc/valkyrieRosControl/sliderBoardControl/PositionJointHolder.java
+++ src/main/java/us/ihmc/valkyrieRosControl/sliderBoardControl/PositionJointHolder.java
@@ -2,6 +2,7 @@ package us.ihmc.valkyrieRosControl.sliderBoardControl;
 
 import us.ihmc.mecano.multiBodySystem.interfaces.OneDoFJointBasics;
 import us.ihmc.rosControl.wholeRobot.PositionJointHandle;
+import us.ihmc.yoVariables.filters.DeltaLimitedYoVariable;
 import us.ihmc.yoVariables.registry.YoRegistry;
 
 /**
