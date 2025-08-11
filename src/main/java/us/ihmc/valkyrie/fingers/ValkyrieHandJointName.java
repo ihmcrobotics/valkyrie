656e81c37f3add850c979f45ee303ba22dd58ff9
Tomasz Bialek <103042423+TomaszTB@users.noreply.github.com>
2025-08-11 14:44:57 -0500

    Moved HandJointName to ihmc-robotics-tools (#55)

--- src/main/java/us/ihmc/valkyrie/fingers/ValkyrieHandJointName.java
+++ src/main/java/us/ihmc/valkyrie/fingers/ValkyrieHandJointName.java
@@ -1,9 +1,9 @@
 package us.ihmc.valkyrie.fingers;
 
-import us.ihmc.humanoidRobotics.communication.packets.dataobjects.HandJointName;
 import us.ihmc.mecano.multiBodySystem.RevoluteJoint;
 import us.ihmc.robotModels.FullRobotModel;
 import us.ihmc.robotics.partNames.FingerName;
+import us.ihmc.robotics.partNames.HandJointName;
 import us.ihmc.robotics.robotSide.RobotSide;
 import us.ihmc.simulationconstructionset.FloatingRootJointRobot;
 import us.ihmc.simulationconstructionset.PinJoint;
