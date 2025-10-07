df7561c756c9f6524af8c0445b540ce1683ff895
Dexton Anderson <danderson@ihmc.org>
2025-10-07 13:56:10 -0500

    Pin to ihmc-open-robotics-software 20251002

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ ihmc {
    configurePublications()
 }
 
-val ihmcOpenRoboticsSoftwareVersion = "source"
+val ihmcOpenRoboticsSoftwareVersion = "20251002"
 
 mainDependencies {
    api("com.martiansoftware:jsap:2.1")
@@ -63,7 +63,7 @@ testDependencies {
 
 rdxDependencies {
    api(ihmc.sourceSetProject("main"))
-   api("us.ihmc:ihmc-high-level-behaviors-libgdx:source")
+   api("us.ihmc:ihmc-high-level-behaviors-libgdx:$ihmcOpenRoboticsSoftwareVersion")
 }
 
 ihmc.jarWithLibFolder()
